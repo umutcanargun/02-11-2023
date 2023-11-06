542d9c861c444eaefb0fd9656966b641f4648eac
umutcanargun <36049614+umutcanargun@users.noreply.github.com>
2023-11-06 14:56:38 +0300

    homework of 2.11.2023
    homework of 2.11.2023

--- flutter_application_3/lib/main.dart
+++ flutter_application_3/lib/main.dart
@@ -1,125 +1,145 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
+import 'package:flutter_application_3/models/quiz_question.dart';
+
+import 'package:flutter_application_3/data/questions.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: StartScreen()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
+            Image.asset("assets/pngwing.com.png"),
             const Text(
-              'You have pushed the button this many times:',
+              "Welcome to Math Quiz",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            OutlinedButton.icon(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const QuestionScreen()),
+                );
+              },
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text("Start"),
+              style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 20)),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// Boilerplate
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({Key? key}) : super(key: key);
+
+  @override
+  _QuestionState createState() => _QuestionState();
+}
+
+class _QuestionState extends State<QuestionScreen> {
+  int initialQuestionIndex = 0;
+  int properlyAnsweredQuestions = 0;
+  void displayQuestions(String selectedOption) {
+    if (initialQuestionIndex < questions.length - 1) {
+      setState(() {
+        initialQuestionIndex = initialQuestionIndex + 1;
+      });
+      if (selectedOption == questions[initialQuestionIndex].trueAnswer) {
+        setState(() {
+          properlyAnsweredQuestions = properlyAnsweredQuestions + 1;
+        });
+      }
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ResultScreen(
+                properlyAnsweredQuestions: properlyAnsweredQuestions)),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(questions[initialQuestionIndex].question),
+          ...questions[initialQuestionIndex].options.map((answer) {
+            return ElevatedButton(
+                onPressed: () {
+                  displayQuestions(answer);
+                },
+                child: Text(
+                  answer,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ));
+          })
+        ]),
+      ),
+    );
+  }
+}
+
+class ResultScreen extends StatelessWidget {
+  final int properlyAnsweredQuestions;
+
+  const ResultScreen({Key? key, required this.properlyAnsweredQuestions})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Quiz is over, $properlyAnsweredQuestions questions were answered correctly!",
+              style: const TextStyle(fontSize: 16, color: Colors.black),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.popUntil(context, (route) => route.isFirst);
+              },
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text(
+                "Start the Quiz Again",
+                style: TextStyle(fontSize: 20),
+              ),
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.black,
+                padding: const EdgeInsets.fromLTRB(25, 20, 25, 20),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
